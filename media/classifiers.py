f6648b0a13cbcfce437dd675fac6d94d8b0ad11e
Jan Wilken Dörrie <dojan@ethz.ch>
2016-05-07 13:24:34 +0200

    New Model

--- media/classifiers.py
+++ media/classifiers.py
@@ -1,62 +1,49 @@
 import csv
 import datetime
 import os
-from collections import Counter
+import sys
 
-import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
 from keras.callbacks import TensorBoard
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
-from keras.layers.core import Activation, Dense, Dropout, Flatten
-from keras.models import Sequential
+from keras.layers import Dense, Dropout, Input, merge
+from keras.models import Model
 from sklearn.cross_validation import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
-from sklearn.metrics import (precision_recall_curve,
-                             precision_recall_fscore_support)
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.naive_bayes import MultinomialNB
-from sklearn.preprocessing import normalize
-from sklearn.svm import SVC
 
 
-def plot_precision_recall(precision, recall):
-    plt.clf()
-    plt.plot(recall, precision, label='Precision-recall curve')
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.05])
-    plt.xlabel('Recall')
-    plt.ylabel('Precision')
-    plt.title('Precision-Recall curve')
-    plt.show()
+def parse_docs_and_tags():
+    titles = []
+    ingredients = []
+    preparations = []
 
-
-def parse_docs_and_tags(fname):
-    docs = []
     tags = []
 
-    with open(fname) as recipes:
-        reader = csv.DictReader(recipes)
-        for line_no, recipe in enumerate(reader):
-            tags.append(recipe['tags'])
-            docs.append(
-                ' '.join([
-                    recipe['title'],
-                    recipe['ingredients']]))
-                    # recipe['preparation']]))
+    reader = csv.DictReader(sys.stdin)
+    for recipe in reader:
+        titles.append(recipe['title'])
+        ingredients.append(recipe['ingredients'])
+        preparations.append(recipe['preparation'])
 
-    tfidf = TfidfVectorizer(stop_words='english', min_df=100)
-    X = tfidf.fit_transform(docs).toarray()
+        tags.append(recipe['tags'])
 
-    counter = CountVectorizer(min_df=100, binary=True, tokenizer=str.split)
+    tfidf = TfidfVectorizer(max_features=5000)
+    tfidf_prep = TfidfVectorizer(max_features=5000, stop_words='english')
+
+    X_title = tfidf.fit_transform(titles).toarray()
+    X_ingredients = tfidf.fit_transform(ingredients).toarray()
+    X_preparation = tfidf_prep.fit_transform(preparations).toarray()
+
+    counter = CountVectorizer(min_df=1, binary=True, tokenizer=str.split)
     y = counter.fit_transform(tags).toarray()
-    # y = (y != 0)
 
     n_labels = y.shape[1]
     tag_index = {idx: tag for tag, idx in counter.vocabulary_.items()}
     tags = np.asarray([tag_index[i] for i in range(n_labels)])
 
-    return X, y, tags
+    return (X_title, X_ingredients, X_preparation), y, tags
 
 
 def classify(X, y):
@@ -69,32 +56,37 @@ def classify(X, y):
 
 
 def print_tags(tags, y_true, y_pred):
-    idx = np.argsort(y_pred, axis=1)
+    # idx = np.argsort(y_pred, axis=1)
     with open('./tags.out', 'w') as f:
         for i in range(y_pred.shape[0]):
-            print(', '.join(list(tags[idx[i, -5:]])), file=f)
+            print(', '.join(list(tags[y_pred[i] > 0.5])), file=f)
 
     with open('./truth.out', 'w') as f:
         for i in range(y_true.shape[0]):
             print(', '.join(tags[y_true[i] != 0.0]), file=f)
 
 
-def dnn(X_train, X_test, y_train, y_test):
-    n_words, n_tags = X_train.shape[1], y_train.shape[1]
-    model = Sequential()
+def dnn(X, y):
+    X_title, X_ingr, X_prep = X
+
+    with tf.name_scope('Input_Merge'):
+        title_input = Input(shape=(X_title.shape[1],), name='title_in')
+        ingr_input = Input(shape=(X_ingr.shape[1],), name='ingr_in')
+        prep_input = Input(shape=(X_prep.shape[1],), name='prep_in')
+
+        X_input = merge([title_input, ingr_input, prep_input], mode='concat')
 
     with tf.name_scope('Dense_Hidden'):
-        model.add(Dense(output_dim=1024, input_dim=n_words))
-        model.add(Activation('relu'))
+        hidden = Dense(output_dim=4096, activation='relu')(X_input)
 
     with tf.name_scope('Dropout'):
-        model.add(Dropout(p=0.5))
+        dropout = Dropout(p=0.5)(hidden)
 
     with tf.name_scope('Dense_Out'):
-        model.add(Dense(n_tags))
-        model.add(Activation('sigmoid'))
+        out = Dense(output_dim=y.shape[1], activation='sigmoid')(dropout)
 
     with tf.name_scope('Optimize_Model'):
+        model = Model(input=[title_input, ingr_input, prep_input], output=out)
         model.compile(
             loss='binary_crossentropy',
             optimizer='SGD',
@@ -109,23 +101,23 @@ def dnn(X_train, X_test, y_train, y_test):
             log_dir=os.path.join('tensorboard', date, time),
             histogram_freq=1)
 
-        model.fit(
-            X_train,
-            y_train,
-            batch_size=50,
-            nb_epoch=3,
-            callbacks=[tsb],
-            verbose=1,
-            validation_data=(X_test, y_test))
+    feed_dict = {'title_in': X_title, 'ingr_in': X_ingr, 'prep_in': X_prep}
+    model.fit(
+        feed_dict,
+        y,
+        batch_size=50,
+        nb_epoch=30,
+        callbacks=[tsb],
+        verbose=1,
+        validation_split=0.7)
 
-    return model.predict_proba(X_test)
+    return model.predict(feed_dict)
 
 
 def main():
-    X, y, tags = parse_docs_and_tags('./allrecipes.csv')
-    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=.7)
-    y_pred = dnn(X_train, X_test, y_train, y_test)
-    print_tags(tags, y_test, y_pred)
+    X, y, tags = parse_docs_and_tags()
+    y_pred = dnn(X, y)
+    print_tags(tags, y, y_pred)
 
 
 if __name__ == "__main__":
